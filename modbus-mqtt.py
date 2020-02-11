72d1f5085a6f26ffcff0fd9d8bba4f92da1a9366
gituser-rk <42944375+gituser-rk@users.noreply.github.com>
2020-02-11 10:28:54 +0100

    code optimization

--- modbus-mqtt.py
+++ modbus-mqtt.py
@@ -90,28 +90,20 @@ def sample_job_every_10s():
         return
 
 def on_connect(client, userdata, flags, rc):
-
     if rc == 0:
         print("Connected to broker")
         global Connected                #Use global variable
         Connected = True                #Signal connection
     else:
-
         print("Connection failed")
-
 client = mqttClient.Client("modbus-mqtt@raspberrypi")   #create new instance
 client.username_pw_set(user, password=password)    #set username and password
 client.on_connect= on_connect                      #attach function to callback
 client.connect(broker_address, port=port)          #connect to broker
-
 client.loop_start()        #start the loop
-
 while Connected != True:    #Wait for connection
     time.sleep(0.1)
-
-
 if __name__ == "__main__":  #main loop
     tl.start(block=True)
-
 client.disconnect()
 client.loop_stop()
