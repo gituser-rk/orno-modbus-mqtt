e642fbd444a8b2dcdd3a987ce5dfdd02f8fe0149
gituser-rk <42944375+gituser-rk@users.noreply.github.com>
2020-02-11 10:26:32 +0100

    improve output in debug mode

--- modbus-mqtt.py
+++ modbus-mqtt.py
@@ -16,7 +16,7 @@ smartmeter.serial.stopbits = 1
 smartmeter.serial.timeout  = 0.10          # seconds
 smartmeter.mode = minimalmodbus.MODE_RTU   # rtu or ascii mode
 smartmeter.clear_buffers_before_each_transaction = True
-smartmeter.debug = False
+smartmeter.debug = False # set to "True" for debug mode
 
 Connected = False #global variable for the state of the connection
 broker_address= "broker.ritz.io"
@@ -25,63 +25,63 @@ user = "user"
 password = "pass"
 
 tl = Timeloop()
-
 @tl.job(interval=timedelta(seconds=10))
 def sample_job_every_10s():
-    value = "{}".format(time.ctime())
-    client.publish("home/energy/solar/datetime",value)
-    f = open("/sys/class/thermal/thermal_zone0/temp", "r")
+    value = "{}".format(time.ctime()) #create timestamp
+    client.publish("home/energy/solar/datetime",value) # publish timestamp
+    f = open("/sys/class/thermal/thermal_zone0/temp", "r") # read Raspberry Pi CPU temperature
     t = f.readline ()
-    t = float(t)/1000   # needs to be converted
-    client.publish("home/energy/solar/RaspPiCpuTemp",t) # publish cpu temperature in degree celtigrade
+    t = float(t)/1000   # convert value
+    client.publish("home/energy/solar/RaspPiCpuTemp",t) # publish cpu temperature in degree celsius
     try:
         Frequency = smartmeter.read_register(304, 2, 3, True)  # registeraddress, number_of_decimals=0, functioncode=3, signed=False
-        #FrequencyTxt = "Die Frequenz ist: %.2f Herz" % Frequency
-        #print (FrequencyTxt)
         client.publish("home/energy/solar/Frequency",Frequency) # publish Frequency in Hz
         Voltage = smartmeter.read_register(305, 2, 3, True)
-        #VoltageTxt = "Die Spannung ist: %.2f Volt" % Voltage
-        #print (VoltageTxt)
         client.publish("home/energy/solar/Voltage",Voltage) # publish Voltage in V
         Current = smartmeter.read_long(313, 3, False, 0) #registeraddress, functioncode=3, signed=False, byteorder=0) in mA
         Current = Current/1000
-        #CurrentTxt = "Die Stromstärke ist: %.3f Ampere" % Current
-        #print (CurrentTxt)
         client.publish("home/energy/solar/Current",Current) # publish Current in A
         ActivePower = smartmeter.read_long(320, 3, False, 0) #registeraddress, functioncode=3, signed=False, byteorder=0)
-        #ActivePowerTxt = "Die Wirkleistung ist: %.2f W" % ActivePower
-        #print (ActivePowerTxt)
         client.publish("home/energy/solar/ActivePower",ActivePower) # publish ActivePower in W
         ReactivePower =  smartmeter.read_long(328, 3, False, 0) #registeraddress, functioncode=3, signed=False, byteorder=0)
-        #ReactivePowerTxt = "Die Blindleistung ist: %.2f Var" % ReactivePower
-        #print (ReactivePowerTxt)
         client.publish("home/energy/solar/ReactivePower",ReactivePower) # publish ReactivePower in Var
         ApparentPower = smartmeter.read_long(336, 3, False, 0) #registeraddress, functioncode=3, signed=False, byteorder=0)
-        #ApparentPowerTxt = "Die Scheinleistung ist: %.2f VA" % ApparentPower
-        #print (ApparentPowerTxt)
         client.publish("home/energy/solar/ApparentPower",ApparentPower) # publish ApparentPower in VA
         PowerFactor = smartmeter.read_register(344, 3, 3, True)
-        #PowerFactorTxt = "Der Leistungsfaktor ist: %.3f " % PowerFactor
-        #print (PowerFactorTxt)
         client.publish("home/energy/solar/PowerFactor",PowerFactor) # publish PowerFactor
         ActiveEnergy = smartmeter.read_registers(40960, 10, 3) #read_registers(registeraddress, number_of_registers, functioncode=3)
-        # Response from meter is: [0, 130, 0, 130, 0, 0, 0, 0, 0, 0]
-        # which means: Total Energy 1.3kWh, T1 Energy 1.3kWh, T2 Energy 0.0kWh, T3 Energy 0.0kWh, T4 Energy 0.0kWh
         bits = (ActiveEnergy[0] << 16) + ActiveEnergy[1] # combining Total Energy valuepair
         s = struct.pack('>i', bits) # write to string an interpret as int
         tmp = struct.unpack('>L', s)[0] # extract from string and interpret as unsigned long
         tmpFloat = tmp/100 # needs to be converted
-        ActiveEnergyTxt = "Zählerstand Energie ist: %.1f kWh" % tmpFloat
-        #print (ActiveEnergyTxt)
         client.publish("home/energy/solar/ActiveEnergy",float(tmpFloat)) # publish ActiveEnergy in kWh
         ReactiveEnergy = smartmeter.read_registers(40990, 10, 3) #read_registers(registeraddress, number_of_registers, functioncode=3)
         bits = (ReactiveEnergy[0] << 16) + ReactiveEnergy[1] # combining Total Energy valuepair
         s = struct.pack('>i', bits) # write to string an interpret as int
         tmp = struct.unpack('>L', s)[0] # extract from string and interpret as unsigned long
         tmpFloat = tmp/100 # needs to be converted
-        ReactiveEnergyTxt = "Zählerstand Blindenergie ist: %.1f kvarh" % tmpFloat
-        #print (ReactiveEnergyTxt)
         client.publish("home/energy/solar/ReactiveEnergy",float(tmpFloat)) # publish ReactiveEnergy in kvarh
+        if smartmeter.debug = True:
+            FrequencyTxt = "Die Frequenz ist: %.2f Herz" % Frequency
+            print (FrequencyTxt)
+            VoltageTxt = "Die Spannung ist: %.2f Volt" % Voltage
+            print (VoltageTxt)
+            CurrentTxt = "Die Stromstärke ist: %.3f Ampere" % Current
+            print (CurrentTxt)
+            ActivePowerTxt = "Die Wirkleistung ist: %.2f W" % ActivePower
+            print (ActivePowerTxt)
+            ReactivePowerTxt = "Die Blindleistung ist: %.2f Var" % ReactivePower
+            print (ReactivePowerTxt)
+            ApparentPowerTxt = "Die Scheinleistung ist: %.2f VA" % ApparentPower
+            print (ApparentPowerTxt)
+            PowerFactorTxt = "Der Leistungsfaktor ist: %.3f " % PowerFactor
+            print (PowerFactorTxt)
+            #Response from meter is: [0, 130, 0, 130, 0, 0, 0, 0, 0, 0]
+            #which means: Total Energy 1.3kWh, T1 Energy 1.3kWh, T2 Energy 0.0kWh, T3 Energy 0.0kWh, T4 Energy 0.0kWh
+            ActiveEnergyTxt = "Zählerstand Energie ist: %.1f kWh" % tmpFloat
+            print (ActiveEnergyTxt)
+            ReactiveEnergyTxt = "Zählerstand Blindenergie ist: %.1f kvarh" % tmpFloat
+            print (ReactiveEnergyTxt)            
         errorcode = "OK"
         client.publish("home/energy/solar/ModbusStatus",errorcode) # publish error status Modbus connection
     except:
